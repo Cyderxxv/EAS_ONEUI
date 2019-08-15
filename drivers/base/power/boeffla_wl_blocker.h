0a7ff7255fcccbbae439fa49633d1f701b5832f8
rifek4 <dzidekck1234@gmail.com>
2019-08-15 17:01:58 +0200

    boeffla_wl_blocker: update default list

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				""
+#define LIST_WL_DEFAULT				"mmc0_detect;wlan_txfl_wake;wlan_rx_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;wlan_wake;wlan_ctrl_wake;ssp_wake_lock;ssp_comm_wake_lock"
 
-#define LENGTH_LIST_WL				1024
+#define LENGTH_LIST_WL				2048
 #define LENGTH_LIST_WL_DEFAULT		(sizeof(LIST_WL_DEFAULT))
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
